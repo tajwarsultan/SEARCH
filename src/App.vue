4bd64366f4ae95c0e9035bc739b8cdc5c6ec436e
tajwar sultan <69696322+tajwarsultan@users.noreply.github.com>
2023-02-01 21:14:54 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-container">
+    <input
+      class="input"
+      v-model="searchTerm"
+      type="text"
+      placeholder="Search"
+    />
+    <div v-if="searchTerm">
+      <p v-for="item in filteredData" :key="item.id">{{ item.title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from "./components/search.json";
+
+export default {
+  data() {
+    return {
+      items: data,
+      searchTerm: "",
+    };
+  },
+  computed: {
+    filteredData() {
+      return this.items.filter((item) => {
+        return item.title.toLowerCase().includes(this.searchTerm.toLowerCase());
+      });
+    },
+  },
+};
+</script>
